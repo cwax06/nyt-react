b09ce4ebe19d49d2dec5f1e273c7271e710b61a2
Andrew Jones <andrew@andrews-mbp-2.uconnect.utah.edu>
2018-03-14 19:03:43 -0600

    working react

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const path = require("path");
+var env = require("dotenv").load();
 const PORT = process.env.PORT || 3001;
 const app = express();
 // set up mongoose and conncting to server
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -12,13 +13,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-mongoose.connect('mongodb://k90armstrong:' + process.env.MONGONYTPASS + '@ds235778.mlab.com:35778/nytreact');
+var test =
+  "mongodb://cwax06:" +
+  process.env.MONGONYTPASS +
+  "@ds215019.mlab.com:15019/nytreact";
+console.log(test);
+
+mongoose.connect(
+  "mongodb://asjones37:" +
+    process.env.MONGONYTPASS +
+    "@ds215019.mlab.com:15019/nytreact"
+);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Send every request to the React app
-require('./api')(app);
+require("./api")(app);
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
